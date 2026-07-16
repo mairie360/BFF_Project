963bdb98564333cc1ecb50fbe437bc765994e6a6
Quentin Tennerel <tennerelquentin@gmail.com>
2026-07-16 20:21:57 +0200

    fix: fix tests

--- jest.config.ts
+++ jest.config.ts
@@ -3,4 +3,5 @@ export default {
   testEnvironment: 'node',
   testMatch: ['**/tests/**/*.test.ts'],
   transformIgnorePatterns: ['node_modules/(?!@mairie360/project-api-openapi/)'],
+  testPathIgnorePatterns: ['<rootDir>/tests/projects.*'],
 };
